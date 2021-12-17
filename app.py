014c7eafb0e9951ac4d5f80ee067cf5607769613
LaneSawyerT <Lanefair@gmail.com>
2021-12-17 11:59:01 +0000

    Add Register template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -31,6 +32,11 @@ def get_artist():
 
     print(artists_combined)
     return render_template("upload.html", artists=artists_combined)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
